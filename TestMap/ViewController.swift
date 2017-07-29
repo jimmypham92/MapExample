332095242b238e3fbe6e850579e198877e748a88
Jimmy.Pham <jimmypham@innovatube.com>
2017-07-29 11:56:53 +0700

    Initial Commit

---
+++ TestMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestMap
+//
+//  Created by Pham Anh on 7/29/17.
+//  Copyright © 2017 Jimmy.Pham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
